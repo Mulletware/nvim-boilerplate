a7627eb11590d4fa8fc68ea02f077e15d0a82ced
Dustin Schultz <dustindschultz@gmail.com>
2024-04-09 14:16:38 -0400

    fixed commands flashing during keypress after RTFM

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,7 +6,7 @@ require 'concessions'
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -20,7 +20,22 @@ map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]ui
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-map('t', '<C-c>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- map('t', '<C-c>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('v', '<C-S-C>', '"*y', { noremap = true, silent = true })
+
+-- System clipboard copy/paste
+-- map({ 'n', 'v' }, '<C-S-C>', function()
+--   vim.fn.setreg('*', vim.fn.getreg '"')
+-- end, { noremap = true, silent = true })
+
+-- map({ 'n', 'v' }, '<C-S-C>', function()
+--   vim.api.nvim_command 'let @+=@"'
+-- end, { noremap = true, silent = true })
+
+-- Use <C-S-V> to paste from system clipboard
+map('n', '<C-S-V>', '"+p', { noremap = true })
+map('i', '<C-S-V>', '<C-r>+', { noremap = true })
 
 -- Tab Navigation
 -- <Leader>1-9 will jump to that tab
@@ -33,27 +48,27 @@ end
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
-map('n', '<C-h>', ':winc h<cr>', { desc = 'Move focus to the left pane' })
-map('n', '<C-l>', ':winc l<cr>', { desc = 'Move focus to the right pane' })
-map('n', '<C-j>', ':winc j<cr>', { desc = 'Move focus to the lower pane' })
-map('n', '<C-k>', ':winc k<cr>', { desc = 'Move focus to the upper pane' })
+map('n', '<C-h>', ':winc h<cr>', { desc = 'Move focus to the left pane', silent = true })
+map('n', '<C-l>', ':winc l<cr>', { desc = 'Move focus to the right pane', silent = true })
+map('n', '<C-j>', ':winc j<cr>', { desc = 'Move focus to the lower pane', silent = true })
+map('n', '<C-k>', ':winc k<cr>', { desc = 'Move focus to the upper pane', silent = true })
 
 -- Window tiling
-map({ 'n', 'i' }, '<C-A-o>', ':only<cr>', { desc = 'Pane becomes the only one' })
-map({ 'n', 'i' }, '<C-A-w>', ':close<cr>', { desc = 'Pane Close' })
+map({ 'n', 'i' }, '<C-A-o>', ':only<cr>', { desc = 'Pane becomes the only one', silent = true })
+map({ 'n', 'i' }, '<C-A-w>', ':close<cr>', { desc = 'Pane Close', silent = true })
 
-map({ 'n', 'i' }, '<C-A-h>', ':vsplit<cr>', { desc = 'Pane open to the right (current file)' })
-map({ 'n', 'i' }, '<C-A-l>', ':vnew<cr>', { desc = 'Pane open to the right' })
-map({ 'n', 'i' }, '<C-A-j>', ':new<cr>', { desc = 'Pane open below' })
-map({ 'n', 'i' }, '<C-A-k>', ':split<cr>', { desc = 'Pane open below' })
+map({ 'n', 'i' }, '<C-A-h>', ':vnew<cr>', { desc = 'Pane open to the right', silent = true })
+map({ 'n', 'i' }, '<C-A-l>', ':vsplit<cr>', { desc = 'Pane open to the right (current file)', silent = true })
+map({ 'n', 'i' }, '<C-A-j>', ':new<cr>', { desc = 'Pane open below', silent = true })
+map({ 'n', 'i' }, '<C-A-k>', ':split<cr>', { desc = 'Pane open below', silent = true })
 
 -- Window tile resizing
-map({ 'n', 'i' }, '<C-S-A-h>', ':vertical resize -2<cr>', { desc = 'Resize vertical pane down' })
-map({ 'n', 'i' }, '<C-S-A-j>', ':resize -2<cr>', { desc = 'Resize horizontal pane down' })
-map({ 'n', 'i' }, '<C-S-A-k>', ':resize +2<cr>', { desc = 'Resize horizontal pane up' })
-map({ 'n', 'i' }, '<C-S-A-l>', ':vertical resize +2<cr>', { desc = 'Resize vertical pane up' })
+map({ 'n', 'i' }, '<C-S-A-h>', ':vertical resize -2<cr>', { desc = 'Resize vertical pane down', silent = true })
+map({ 'n', 'i' }, '<C-S-A-j>', ':resize -2<cr>', { desc = 'Resize horizontal pane down', silent = true })
+map({ 'n', 'i' }, '<C-S-A-k>', ':resize +2<cr>', { desc = 'Resize horizontal pane up', silent = true })
+map({ 'n', 'i' }, '<C-S-A-l>', ':vertical resize +2<cr>', { desc = 'Resize vertical pane up', silent = true })
 
-map('n', '<Leader>q', ':close<cr>', { desc = 'Close window' })
+map('n', '<Leader>q', ':close<cr>', { desc = 'Close window', silent = true })
 
 -- Custom mappings
 
@@ -62,30 +77,30 @@ map('n', ';', ':', { desc = 'CMD enter command mode' })
 -- Generic Text Editor mappings --
 
 -- map Ctrl + s to Save
-map({ 'n', 'i' }, '<C-s>', ':w<CR>', { desc = 'Save File' })
+map({ 'n', 'i' }, '<C-s>', ':w<CR>', { desc = 'Save File', silent = true })
 
 -- map Ctrl+F to telescope live_grep
 map({ 'n', 'i' }, '<C-f>', '<cmd>Telescope live_grep<cr>', { desc = 'Find Text in Files (Normal Mode)' })
 
 -- map Ctrl + w to close Buffer/tab
-map({ 'i', 'n' }, '<C-w>', ':bd<cr>', { desc = 'Close File' })
-map({ 'i', 'n' }, '<C-S-w>', ':bd!<cr>', { desc = 'Close File (forced)' })
+map({ 'i', 'n' }, '<C-w>', ':bd<cr>', { desc = 'Close File', silent = true })
+map({ 'i', 'n' }, '<C-S-w>', ':bd!<cr>', { desc = 'Close File (forced)', silent = true })
 
 -- map Ctrl + t to new buffer
-map({ 'n', 'i' }, '<C-t>', '<cmd>enew<cr>', { desc = 'New Buffer' })
+map({ 'n', 'i' }, '<C-t>', '<cmd>enew<cr>', { desc = 'New Buffer', silent = true })
 
 -- map Ctrl + q to quit (press enter to confirm, add ! to force)
-map({ 'n', 'i' }, '<C-q>', '<cmd>qa<cr>', { desc = 'Close Application' })
-map({ 'n', 'i' }, '<C-S-q>', '<cmd>qa!<cr>', { desc = 'Close Application without saving' })
+map({ 'n', 'i' }, '<C-q>', '<cmd>qa<cr>', { desc = 'Close Application', silent = true })
+map({ 'n', 'i' }, '<C-S-q>', '<cmd>qa!<cr>', { desc = 'Close Application without saving', silent = true })
 
 -- map Ctrl + ] and Ctrl + [ to indent/unindent code
-map({ 'n', 'i' }, '<C-]>', '>>', 'Indent')
-map({ 'n', 'i' }, '<C-[>', '<<', 'Indent Backward')
-map('v', '<C-[>', '<gv', 'Indent Backward (Visual Mode)')
-map('v', '<C-]>', '>gv', 'Indent (Visual Mode)')
+map({ 'n', 'i' }, '<C-]>', '>>', { desc = 'Indent', silent = true })
+map({ 'n', 'i' }, '<C-[>', '<<', { desc = 'Indent Backward', silent = true })
+map('v', '<C-[>', '<gv', { desc = 'Indent Backward (Visual Mode)', silent = true })
+map('v', '<C-]>', '>gv', { desc = 'Indent (Visual Mode)', silent = true })
 
 -- map Ctrl+Shift+t to reopen last closed tab with LastBuf
-map({ 'n', 'i' }, '<C-S-t>', ':LastBuf', { desc = 'Reopen last closed tab' })
+map({ 'n', 'i' }, '<C-S-t>', ':LastBuf', { desc = 'Reopen last closed tab', silent = true })
 
 -- map Ctrl + b to Neotree open
 map({ 'n', 'i' }, '<C-b>', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle Neotree (Normal Mode)' })
@@ -98,7 +113,7 @@ map({ 'n', 'i' }, '<C-p>', '<cmd>Telescope find_files<cr>', { desc = 'Find Files
 map({ 'n', 'i' }, '<C-S-p>', ':Telescope buffers<cr>', { desc = 'Fuzzy Find In Open Buffers' })
 
 -- map Ctrl+A to select all
-map({ 'n', 'i' }, '<C-a>', '<cmd>normal ggVG<cr>', { desc = 'Select all (Normal Mode)' })
+map({ 'n', 'i' }, '<C-a>', '<cmd>normal ggVG<cr>', { desc = 'Select all (Normal Mode)', silent = true })
 
 -- map Ctrl + / to comment line
 
@@ -106,16 +121,16 @@ local comment = function()
   require('Comment.api').toggle.linewise.current()
 end
 
-map('n', '<C-/>', comment, { noremap = true, desc = 'Comment line (Normal mode)' })
-map('i', '<C-/>', comment, { noremap = true, desc = 'Comment line (Visual Mode)' })
-map('v', '<C-/>', '<ESC><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>gv=gv', 'Comment line (Visual Mode)')
+map('n', '<C-/>', comment, { noremap = true, desc = 'Comment line (Normal mode)', silent = true })
+map('i', '<C-/>', comment, { noremap = true, desc = 'Comment line (Visual Mode)', silent = true })
+map('v', '<C-/>', '<ESC><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>gv=gv', { desc = 'Comment line (Visual Mode)', silent = true })
 
 map('v', '<C-A-/>', '<C-\\><C-N><Cmd>lua require("Comment.api").toggle.blockwise(vim.fn.visualmode())<CR>', { noremap = true, silent = true })
 
 map('v', '<C-S>?', function()
   require('Comment.api').toggle.blockwise(vim.fn.visualmode())
   vim.cmd 'normal! gv=gv'
-end, 'Comment line (Visual Mode)')
+end, { desc = 'Comment line (Visual Mode)', silent = true })
 
 map(
   'v',
