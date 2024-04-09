f90d18310c3d442f2f914430314a15b9618c17ad
Dustin Schultz <dustindschultz@gmail.com>
2024-04-09 14:06:29 -0400

    moved control of clipboard style from main to user config

--- lua/user/config.lua
+++ lua/user/config.lua
@@ -1,3 +1,8 @@
 -- add your personal keymaps here to avoid conflicts
 --   do not check changes to this file into the upstream branch!
 -- local map = require '../utils.map'
+
+-- Sync clipboard between OS and Neovim.
+--  Uncomment this option if you want your OS clipboard to sync with Neovim.
+--  See `:help 'clipboard'`
+-- vim.opt.clipboard = 'unnamedplus'
