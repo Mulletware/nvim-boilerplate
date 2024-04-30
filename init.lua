2fa9d11ec046a477ae47963ea33b87990ce3691a
Dustin Schultz <dustindschultz@gmail.com>
2024-04-30 13:59:46 -0400

    fixed issue with telescope.close keybinding

--- init.lua
+++ init.lua
@@ -277,7 +277,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<Esc>', require('telescope.actions').close, { desc = 'Close Telescope' })
+      vim.keymap.set('n', '<Esc>', "lua require('telescope.actions').close()", { desc = 'Close Telescope' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
