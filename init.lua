bb55c822a6f859c7ab9a535736354a7d86994fbc
Dustin Schultz <dustindschultz@gmail.com>
2024-06-10 08:31:18 -0400

    added hidden files to telescope find_files results

--- init.lua
+++ init.lua
@@ -243,7 +243,11 @@ require('lazy').setup({
             },
           },
         },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
