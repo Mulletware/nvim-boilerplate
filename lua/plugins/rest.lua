35d9dd68f41ac06535372907b1c8591bdde3eaeb
Dustin Schultz <dustindschultz@gmail.com>
2025-01-26 21:04:19 -0500

    added auto-env to rest.nvim

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,18 +1,26 @@
 return {
-  -- {
-  --   'vhyrro/luarocks.nvim',
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' },
-  --   },
-  -- },
   {
     'rest-nvim/rest.nvim',
     ft = 'http',
     dependencies = { 'luarocks.nvim' },
     config = function()
       vim.g.rest_nvim = {}
+
+      require('telescope').load_extension 'rest'
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('rest_nvim', { clear = true }),
+        pattern = 'http',
+        callback = function()
+          local lines = require('rest-nvim.dotenv').find_env_files()
+
+          if #lines > 0 then
+            vim.defer_fn(function()
+              require('telescope').extensions.rest.select_env()
+            end, 0)
+          end
+        end,
+      })
     end,
   },
 }
