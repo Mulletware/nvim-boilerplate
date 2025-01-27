805db659e0ae138b3f670d790085135a0433d86d
Dustin Schultz <dustindschultz@gmail.com>
2025-01-26 21:07:25 -0500

    added scrolling distance convenience fn

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,1 +1,17 @@
+local set_scroll = function(divisor)
+  vim.cmd('execute "set scroll=' .. math.floor(vim.api.nvim_win_get_height(0) / divisor) .. '"')
+end
+
+vim.g.set_scroll = function(divisor)
+  set_scroll(divisor)
+
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'VimResized', 'BufNew', 'WinScrolled' }, {
+    desc = 'Set scroll height on window enter',
+    group = vim.api.nvim_create_augroup('scroll-height-enter', { clear = true }),
+    callback = function()
+      set_scroll(divisor)
+    end,
+  })
+end
+
 require 'user.autocmd'
