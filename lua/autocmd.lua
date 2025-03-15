c8c68d2c115c8f15abc679bfd9993793b7f4c547
Dustin Schultz <dustindschultz@gmail.com>
2025-03-15 16:13:55 -0400

    updated set_scroll fn to just accept the raw number of lines

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,17 +1,5 @@
-local set_scroll = function(divisor)
-  vim.cmd('execute "set scroll=' .. math.floor(vim.api.nvim_win_get_height(0) / divisor) .. '"')
-end
-
-vim.g.set_scroll = function(divisor)
-  set_scroll(divisor)
-
-  vim.api.nvim_create_autocmd({ 'BufEnter', 'VimResized', 'BufNew', 'WinScrolled' }, {
-    desc = 'Set scroll height on window enter',
-    group = vim.api.nvim_create_augroup('scroll-height-enter', { clear = true }),
-    callback = function()
-      set_scroll(divisor)
-    end,
-  })
+vim.g.set_scroll = function(lines)
+  vim.cmd('execute "set scroll=' .. lines .. '"')
 end
 
 vim.cmd [[autocmd FileType markdown set tw=80|set wrap]]
