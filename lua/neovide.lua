c10d19dece9afe7de9a541dc7ba56fb44532deb8
Dustin Schultz <dustindschultz@gmail.com>
2025-03-17 11:16:13 -0400

    new lua formatter wanted to make some changes

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -4,7 +4,7 @@
 local map = require 'utils.map'
 
 if vim.g.neovide then
-  map('v', '<C-S-C>', '"+y', { desc = 'Copy', silent = true }) -- Copy
+  map('v', '<C-S-C>', '"+y', { desc = 'Copy', silent = true })                -- Copy
   map({ 'n', 'i', 'v' }, '<C-S-V>', '"+p', { desc = 'Paste', silent = true }) -- Paste normal mode
 
   local scaling_factor = 1.01
@@ -13,7 +13,10 @@ if vim.g.neovide then
   local last_scale_notify_id = nil
 
   function show_scale_factor()
-    last_scale_notify_id = vim.notify(string.format('Scale: %.2f', vim.g.neovide_scale_factor), vim.log.levels.INFO, { replace = last_scale_notify_id })
+    last_scale_notify_id = vim.notify(string.format('Scale: %.2f', vim.g.neovide_scale_factor),
+      vim.log.levels.INFO,
+      { replace = last_scale_notify_id }
+    )
   end
 
   function increase_scale_factor()
@@ -28,12 +31,13 @@ if vim.g.neovide then
 
   map('n', '<C-=>', increase_scale_factor, { desc = 'Zoom in', silent = true })
   map('n', '<C-->', decrease_scale_factor, { desc = 'Zoom out', silent = true })
-  map({ 'n', 'i' }, '<A-ScrollWheelUp>', increase_scale_factor, { desc = 'Zoom in', silent = true })
-  map({ 'n', 'i' }, '<A-ScrollWheelDown>', decrease_scale_factor, { desc = 'Zoom out', silent = true })
+  map({ 'n', 'i' }, '<S-A-ScrollWheelUp>', increase_scale_factor, { desc = 'Zoom in', silent = true })
+  map({ 'n', 'i' }, '<S-A-ScrollWheelDown>', decrease_scale_factor, { desc = 'Zoom out', silent = true })
 
   -- since we're in neovide we can leverage the full Ctrl+f and Ctrl+Shift+f search functionality normally intercepted by the shell
   map({ 'n', 'i' }, '<C-S-f>', '<cmd>Telescope live_grep<cr>', { desc = 'Find Text in Files (Normal Mode)' })
-  map({ 'n', 'i' }, '<C-f>', '<cmd>Telescope current_buffer_fuzzy_find<cr>', { desc = 'Find Text in Files (Normal Mode)' })
+  map({ 'n', 'i' }, '<C-f>', '<cmd>Telescope current_buffer_fuzzy_find<cr>',
+    { desc = 'Find Text in Files (Normal Mode)' })
 
   map({ 'n', 'i' }, '<C-tab>', '<cmd>bnext<cr>', 'Next Buffer/tab')
   map({ 'n', 'i' }, '<C-S-tab>', '<cmd>bprevious<cr>', 'Next Buffer/tab')
