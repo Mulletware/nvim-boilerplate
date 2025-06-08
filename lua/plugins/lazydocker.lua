57d0363e7a2413960f21fe8f0f38aa6f2d0d4476
Dustin Schultz <dustindschultz@gmail.com>
2025-06-08 18:04:13 +0000

    swapped out lazydocker projects for another

--- lua/plugins/lazydocker.lua
+++ lua/plugins/lazydocker.lua
@@ -1,17 +1,4 @@
 return {
   'crnvl96/lazydocker.nvim',
-  event = 'VeryLazy',
-  opts = {
-    popup_window = {
-      position = '0%',
-      relative = 'editor',
-      size = {
-        width = '100%',
-        height = '100%',
-      },
-    },
-  }, -- automatically calls `require("lazydocker").setup()`
-  dependencies = {
-    'MunifTanjim/nui.nvim',
-  },
+  opts = {},
 }
